b9fd975cae73d88542b59126171ce9b729a06901
kotawindows <kotawonderlandjapan@outlook.jp>
2020-12-01 11:02:06 +0900

    fetchTicker error handling

--- btc-jpy.js
+++ btc-jpy.js
@@ -6,7 +6,7 @@ const btcPrice = [];
 const numberOfSheets = 0.001; //1回の取引で使うBTC枚数
 
 const sleep = () => {
-  return new Promise((resolve) => { setTimeout(() => { resolve(); }, 30000); });
+  return new Promise((resolve) => { setTimeout(() => { resolve(); }, 60000); });
 }
 
 (async function () {
@@ -17,8 +17,12 @@ const sleep = () => {
       const ticker = await bitflyer.fetchTicker('BTC/JPY');
       btcPrice.push(ticker.ask);
     } catch (e) {
-      console.log("await bitflyer.fetchTicker('BTC/JPY');できませんでした");
-      return true;
+      //10時間ほど経つとfetchTicker関数は接続エラーを起こすためエラーハンドリング
+      setTimeout(function () {
+        console.log("await bitflyer.fetchTicker('BTC/JPY');できませんでした");
+        console.log(e);
+        continue;
+      }, 10000)
     }
     if (btcPrice.length > 4) {
       btcPrice.shift();
@@ -26,21 +30,21 @@ const sleep = () => {
     console.log(btcPrice);
     // 上昇を検知
     if (btcPrice[3] > btcPrice[2] && btcPrice[2] > btcPrice[1] && btcPrice[1] > btcPrice[0]) {
-        try {
-          await bitflyer.createMarketSellOrder('BTC/JPY', numberOfSheets);
-          console.log(`上昇傾向を検知,${numberOfSheets}BTC売りました`);
-        } catch (e) {
-          console.log("上昇傾向を検知しましたがBTCがないため売ることができませんでした");
-        }
+      try {
+        await bitflyer.createMarketSellOrder('BTC/JPY', numberOfSheets);
+        console.log(`上昇傾向を検知,${numberOfSheets}BTC売りました`);
+      } catch (e) {
+        console.log("上昇傾向を検知しましたがBTCがないため売ることができませんでした");
+      }
     }
     // 下降を検知
     if (btcPrice[3] < btcPrice[2] && btcPrice[2] < btcPrice[1] && btcPrice[1] < btcPrice[0]) {
-        try {
-          await bitflyer.createMarketBuyOrder('BTC/JPY', numberOfSheets);
-          console.log(`下降傾向を検知,${numberOfSheets}BTC買いました`)
-        } catch (e) {
-          console.log("下降傾向を検知しましたがJPYがないため買うことができませんでした");
-        }
+      try {
+        await bitflyer.createMarketBuyOrder('BTC/JPY', numberOfSheets);
+        console.log(`下降傾向を検知,${numberOfSheets}BTC買いました`)
+      } catch (e) {
+        console.log("下降傾向を検知しましたがJPYがないため買うことができませんでした");
+      }
     }
     await sleep();
   }
