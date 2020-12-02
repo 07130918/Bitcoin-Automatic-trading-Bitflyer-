35689e0b4918d6f3e29364e3e826858c60b865a2
kotawindows <kotawonderlandjapan@outlook.jp>
2020-12-02 10:04:58 +0900

    newError handling

--- btc-jpy.js
+++ btc-jpy.js
@@ -4,9 +4,10 @@ const key = require('./key');
 
 const btcPrice = [];
 const numberOfSheets = 0.001; //1回の取引で使うBTC枚数
+const interval = 1000;
 
-const sleep = () => {
-  return new Promise((resolve) => { setTimeout(() => { resolve(); }, 60000); });
+const sleep = (interval) => {
+  return new Promise((resolve) => { setTimeout(() => { resolve(); }, interval); });
 }
 
 (async function () {
@@ -16,36 +17,36 @@ const sleep = () => {
     try {
       const ticker = await bitflyer.fetchTicker('BTC/JPY');
       btcPrice.push(ticker.ask);
+      if (btcPrice.length > 4) {
+        btcPrice.shift();
+      }
+      console.log(btcPrice);
+      // 上昇を検知
+      if (btcPrice[3] > btcPrice[2] && btcPrice[2] > btcPrice[1] && btcPrice[1] > btcPrice[0]) {
+        try {
+          await bitflyer.createMarketSellOrder('BTC/JPY', numberOfSheets);
+          console.log(`上昇傾向を検知,${numberOfSheets}BTC売りました`);
+        } catch (e) {
+          console.log("上昇傾向を検知しましたがBTCがないため売ることができませんでした");
+        }
+      }
+      // 下降を検知
+      if (btcPrice[3] < btcPrice[2] && btcPrice[2] < btcPrice[1] && btcPrice[1] < btcPrice[0]) {
+        try {
+          await bitflyer.createMarketBuyOrder('BTC/JPY', numberOfSheets);
+          console.log(`下降傾向を検知,${numberOfSheets}BTC買いました`)
+        } catch (e) {
+          console.log("下降傾向を検知しましたがJPYがないため買うことができませんでした");
+        }
+      }
     } catch (e) {
       //10時間ほど経つとfetchTicker関数は接続エラーを起こすためエラーハンドリング
       let date = new Date().toLocaleString({ timeZone: 'Asia/Tokyo' });
       console.log("await bitflyer.fetchTicker('BTC/JPY');でエラー発生", e);
       console.log(`エラー発生時刻: ${date}`);
-      await sleep();
+      await sleep(interval);
       continue;
     }
-    if (btcPrice.length > 4) {
-      btcPrice.shift();
-    }
-    console.log(btcPrice);
-    // 上昇を検知
-    if (btcPrice[3] > btcPrice[2] && btcPrice[2] > btcPrice[1] && btcPrice[1] > btcPrice[0]) {
-      try {
-        await bitflyer.createMarketSellOrder('BTC/JPY', numberOfSheets);
-        console.log(`上昇傾向を検知,${numberOfSheets}BTC売りました`);
-      } catch (e) {
-        console.log("上昇傾向を検知しましたがBTCがないため売ることができませんでした");
-      }
-    }
-    // 下降を検知
-    if (btcPrice[3] < btcPrice[2] && btcPrice[2] < btcPrice[1] && btcPrice[1] < btcPrice[0]) {
-      try {
-        await bitflyer.createMarketBuyOrder('BTC/JPY', numberOfSheets);
-        console.log(`下降傾向を検知,${numberOfSheets}BTC買いました`)
-      } catch (e) {
-        console.log("下降傾向を検知しましたがJPYがないため買うことができませんでした");
-      }
-    }
-    await sleep();
+    await sleep(interval);
   }
 })();
